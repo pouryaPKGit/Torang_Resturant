3f9dbcdb77272bc4f938366c7589a06f3b5fab9f
pourya pourkhani <pourya.pourkhani.git@gmail.com>
2024-09-23 19:59:09 +0330

    Update MyWebsite.jsx

--- src/Components/MyWebsite.jsx
+++ src/Components/MyWebsite.jsx
@@ -4,7 +4,7 @@ const MyWebsite = () => {
   return (
     <div className='w-[89%] h-[80px] bg-[#183D3D] m-auto rounded-2xl mt-2'>
         <div className='flex items-center justify-center text-green-200  text-md font-bold  pt-5'>
-     <p>* | Developede By : Pourya Pourkhani | *</p>
+     <p>* | Developed By : Pourya Pourkhani | *</p>
       </div>
     </div>
   )
